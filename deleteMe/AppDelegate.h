d2033c9dae69449c75d3aad931714c14600a0e24
Ian Jenkins <ianjerodjenkins@gmail.com>
2015-09-10 15:16:02 -0500

    first commit

--- deleteMe/AppDelegate.h
+++ deleteMe/AppDelegate.h
@@ -4,7 +4,7 @@
 //
 //  Created by Ian Jenkins on 9/10/15.
 //  Copyright (c) 2015 Ian Jenkins. All rights reserved.
-//
+//  Random
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
--- deleteMe/AppDelegate.m
+++ deleteMe/AppDelegate.m
@@ -4,7 +4,7 @@
 //
 //  Created by Ian Jenkins on 9/10/15.
 //  Copyright (c) 2015 Ian Jenkins. All rights reserved.
-//
+//  Random 2
 
 #import "AppDelegate.h"
 
